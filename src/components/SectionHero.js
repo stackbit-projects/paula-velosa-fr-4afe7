4776d94633c52428c8cfc10972b63ca766550c49
Stackbit Code Editor <projects@stackbit.com>
2021-07-08 00:40:03 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -9,17 +9,16 @@ export default class SectionHero extends React.Component {
         const section = _.get(this.props, 'section');
         const sectionId = _.get(section, 'section_id');
         const title = _.get(section, 'title');
-        const subtitle = _.get(section, 'subtitle');
         const content = _.get(section, 'content');
         const actions = _.get(section, 'actions');
 
         return (
             <section id={sectionId} className="block block-hero outer outer--main">
                 <div className="inner">
-                    {(title || subtitle) && (
-                        <div className="block-header inner-sm">
-                            {title && <h2 className="block-title line-top">{title}</h2>}
-                            {subtitle && <p className="block-subtitle">{htmlToReact(subtitle)}</p>}
+                    {title && (
+                        <div className="block-header inner-sm inner--main ">
+                            <h1 className="block-title">{title}</h1>
+
                         </div>
                     )}
                     {content && <div className="block-content inner-sm inner--main">{markdownify(content)}</div>}
