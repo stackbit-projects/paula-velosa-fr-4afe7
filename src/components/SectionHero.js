1f4d57d5514e8c899cd212ed64e96d5bee86d08a
Stackbit Code Editor <projects@stackbit.com>
2021-07-08 00:27:46 +0000

    SectionHero.js: changed

--- src/components/SectionHero.js
+++ src/components/SectionHero.js
@@ -26,7 +26,18 @@ export default class SectionHero extends React.Component {
                             <CtaButtons actions={actions} />
                         </div>
                     )}
-                    
+                                    <div className="inner">
+                    <div className="site-footer-inside">
+                        <div className="site-info">
+                            {copyright && <span className="copyright">{htmlToReact(copyright)}</span>}
+                            {_.map(links, (action, index) => <ActionLink key={index} action={action} />)}
+                        </div>
+                        {hasSocial && !_.isEmpty(socialLinks) && (
+                            <div className="social-links">
+                                {_.map(socialLinks, (action, index) => <Action key={index} action={action} />)}
+                            </div>
+                        )}
+                    </div>
                 </div>
             </section>
         );
